bcf92dfb0f6960c7bebc45ce505d8fe76249df4e
Christian Alexander <christian.alexanderato@gmail.com>
2024-01-02 16:15:24 -0800

    setup connection

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 // DEPENDENCIES
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const app = express();
+
+// Database connection
+const DATABASE_URL = process.env.DATABASE_URL;
+mongoose.connect(DATABASE_URL);
+mongoose.connection.on("error", (error) => console.log(error));
+mongoose.connection.on("open", () => console.log("connected to mongo"));
+mongoose.connection.on("close", () => console.log("Disconnected from mongo"));
 
 // MIDDLE WARE
 app.use(morgan("dev"));
